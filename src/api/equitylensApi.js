6ef2519f93b543645c2652aeaa0c4f31bc094bfc
nicholaspapazis <nicholasepapazis@gmail.com>
2026-05-26 18:30:56 -0400

    modify basic layout, and add comments

--- src/api/equitylensApi.js
+++ src/api/equitylensApi.js
@@ -3,12 +3,13 @@ This is where the frontend will talk to the backend.
 */
 
 
-import axios from "axios";
+import axios from "axios"; //need for api calls
 
-const API_BASE = "http://localhost:8000";
+const API_BASE = "http://localhost:8000"; //access to the equitylensApi
 
+//get stock data for any ticker
 export const getStockData = async (ticker) => {
-    const res = await axios.get(`${API_BASE}/stock/${ticker}`);
-    return res.data;
+    const res = await axios.get(`${API_BASE}/stock/${ticker}`); //make the api call
+    return res.data; 
 
 }
--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -2,29 +2,48 @@ import { useState } from "react";
 import { getStockData } from "../api/equitylensApi";
 
 export default function Dashboard() {
-  const [ticker, setTicker] = useState("AAPL");
-  const [data, setData] = useState(null);
 
+  const [ticker, setTicker] = useState("AAPL");
+  const [data, setData] = useState(null); //state variable to hold current stock data
+
+  //get the stock data for specified ticker
   const fetchData = async () => {
-    const result = await getStockData(ticker);
+    const result = await getStockData(ticker); //call method from equitylensApi.js
     setData(result);
   };
 
   return (
-    <div style={{ padding: "20px" }}>
+    <div style={{ padding: "24px", fontFamily: "Arial" }}>
       <h1>EquityLens</h1>
 
-      <input
-        value={ticker}
-        onChange={(e) => setTicker(e.target.value)}
-        placeholder="Enter ticker"
-      />
+      {/* ticker input field */}  
+      <div style={{ marginBottom: "20px" }}>
+        <input
+            value={ticker}
+            onChange={(e) => setTicker(e.target.value)}
+            placeholder="Enter ticker (e.g. AAPL)"
+            style={{ padding: "8px", marginRight: "10px" }}
+        />
 
-      <button onClick={fetchData}>Search</button>
+        {/* Analyze button */}
+        <button onClick={fetchData}>
+            Analyze
+        </button>
+      </div>
 
-      {data && (
-        <pre>{JSON.stringify(data, null, 2)}</pre>
-      )}
+      {/* Main Content */}
+      <div style={{ display: "grid", gridTemplateColumns: "1fr", gap: "20px" }}>
+        
+        {/* Conditional render data if it is truthy */}
+        {data && (
+            <div>
+                <h3>{data.ticker}</h3>
+                <p>Data points: {data.data.length}</p>
+            </div>
+        )}
+
+      </div>
+      
     </div>
   );
 }
