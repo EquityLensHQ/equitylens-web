b0e6234364fc8378af923ae5deaebe1a17aa22c6
nicholaspapazis <nicholasepapazis@gmail.com>
2026-06-28 18:22:34 -0400

    Implement rsi endpoint

--- src/pages/MacroStats.jsx
+++ src/pages/MacroStats.jsx
@@ -1,22 +1,93 @@
 import { useEffect, useState } from "react";
-import "./MarketOverview.css";
-import { getOverview } from "../api/overviewApi";
+import "./MacroStats.css";
+import { getMacroStats } from "../api/macroStatsApi";
 import Navbar from "../components/Navbar";
+import RsiCard from "../components/RsiCard";
+
 
 
 export default function MarketOverview() {
-  
+
+    const [macroData, setMacroData] = useState(null);
+
+    useEffect(() => {
+
+        async function loadData(){
+
+            const response = await getMacroStats();
+
+            setMacroData(response.data);
+
+        }
+
+
+        loadData();
+
+
+    }, []);
+
+
+
+    if (!macroData){
+
+        return (
+            <div className="market-page">
+                Loading...
+            </div>
+        )
+
+    }
+
     return (
+
         <div className="market-page">
+
             <div className="container">
 
-            <Navbar />
 
-            <h1 className="title">Macro Stats</h1>
+                <Navbar />
+
+
+                <h1 className="title">
+                    Macro Stats
+                </h1>
+
+                <p>Short-term momentum</p>
+
+                <div className="rsi-grid">
+
+
+                    <RsiCard
+
+                        title="Highest RSI-2"
+
+                        data={macroData.top_rsi}
+
+                        type="top"
+
+                    />
+
+
+
+                    <RsiCard
+
+                        title="Lowest RSI-2"
+
+                        data={macroData.bottom_rsi}
+
+                        type="bottom"
+
+                    />
+
+
+                </div>
 
 
             </div>
+
+
         </div>
+
     );
 
 }
